cea3ac02208678a888a0c55b7aac60d7cf544769
dani brake <ofloveandhate@users.noreply.github.com>
2011-10-13 17:31:26 +0000

    added master functions header

---
+++ polysaurus/trunk/master.h
@@ -0,0 +1,38 @@
+#include <mpi.h>
+#include "step1.h"
+#include "step2.h"
+#include "mtrand.h"
+#include "random.h"
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h> 
+#include <vector>
+#include <map>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include "step2readandwrite.h"
+
+#ifndef __MASTER_H__
+#define __MASTER_H__
+
+void master(std::vector<std::string> dir,
+			std::string filename,
+			int numfilesatatime,
+			int saveprogresseverysomany);
+
+void FormNextValues(int numfilesatatime,
+						int userdefined,
+						int numparam,
+						int localcounter,	
+						std::vector< std::vector< std::pair<double,double> > > Values,
+						int countingup,
+					  std::vector< int > KVector,
+						double tempsends[]);
+	
+
+#endif
