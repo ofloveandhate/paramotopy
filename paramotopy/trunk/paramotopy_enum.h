774214ff00fbc1616032f63b862ffee778aca920
brake <brake@f9a24756-1822-44c4-9423-d39220829b31>
2012-02-08 06:04:44 +0000

    so that the enum can be used in multiple functions

---
+++ paramotopy/trunk/paramotopy_enum.h
@@ -0,0 +1,7 @@
+
+#ifndef __PARA_ENUM__
+#define __PARA_ENUM__
+
+enum OPTIONS {Start, Input, SetRandom, SaveRandom, LoadRandom, WriteStepOne, RunStepOne, Step2,CollectData ,DetPrefs, Quit};
+
+#endif
