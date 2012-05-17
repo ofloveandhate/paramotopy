2e711b85d30f1baccb6196a8deb806ebb6d43e01
dani brake <ofloveandhate@users.noreply.github.com>
2012-05-17 17:10:04 +0000

    made a file for the preferences functions

---
+++ preferences.h
@@ -0,0 +1,40 @@
+#include <vector>
+#include <map>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <stdio.h>
+#include "random.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h> 
+#include <sstream>
+#include <fstream>
+#include <string>
+#include "para_aux_funcs.h"
+
+#ifndef __PREFS_H__
+#define __PREFS_H__
+
+
+
+
+
+bool DeterminePreferences(preferences *Prefs, bool rerun, std::vector< bool > & FilePrefVector);
+/*
+ read the user's preferences for parallelism on the machine.
+ @param rerun - a bool to flag redetermining the preferences
+ */
+
+
+void SetPrefVersion(int version);
+
+int GetPrefVersion();
+
+
+#endif
+
