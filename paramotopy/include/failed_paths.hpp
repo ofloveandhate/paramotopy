f01d46cf2969fe9b7de68eee93980a967c453416
niemerg <niemerg@f9a24756-1822-44c4-9423-d39220829b31>
2013-03-06 14:34:30 +0000

    added doxygen style comments - men

--- paramotopy/include/failed_paths.hpp
+++ paramotopy/include/failed_paths.hpp
@@ -31,56 +31,115 @@
 
 //the case to be called from paramotopy
 int failedpaths_case(runinfo & paramotopy_info, 
-					 ProgSettings & paramotopy_settings,
-					 int & iteration);
+		     ProgSettings & paramotopy_settings,
+		     int & iteration);
 
 
 
 
-
+/** A class for the failed path analysis. */
 class failinfo {
 public:
 
-	//VARIABLES
-	int num_points_inspected;
-
-	std::vector< point > master_fails;
-	std::map< int, std::vector < point > > initial_fails;
-	std::map< int, std::vector < point > > terminal_fails;
-	
-	int totalfails;
-	std::vector< std::string > foldervector;
-	int current_iteration;
-	
-	//FUNCTIONS
-	
-	void MainMenu(ProgSettings & paramotopy_settings, runinfo & paramotopy_info);
-
-	
+  //VARIABLES
+  /** The number of points to test if there was a failed path. */
+  int num_points_inspected;
+  /** The parameter points that had failed paths. */
+  std::vector< point > master_fails;
+  /** The parameter points that had initial files. */
+  std::map< int, std::vector < point > > initial_fails;
+  /** The parameter points that keep on failing. */
+  std::map< int, std::vector < point > > terminal_fails;
+  
+  int totalfails;
+  std::vector< std::string > foldervector;
+  int current_iteration;
+  
+  //FUNCTIONS
+  /** Display the main menu of the failed paths analysis. 
+      \param paramotopy_settings - The current program preference settings.
+      \param paramotopy_info - The current run information.
+  */
+  void MainMenu(ProgSettings & paramotopy_settings, runinfo & paramotopy_info);
+  
+  
 private:
-	
-	
-	void StartOver(runinfo & paramotopy_info);
-	void RecoverProgress(runinfo & paramotopy_info);
-	void PerformAnalysis(ProgSettings & paramotopy_settings, runinfo & paramotopy_info);
-	
-	void set_location_fail(runinfo & paramotopy_info);
-	
-	void report_restored_data();
-	int find_failed_paths(runinfo paramotopy_info, int temporal_flag, int iteration);
-	void get_folders_for_fail(std::string dir);
-	std::string new_step_one(ProgSettings & paramotopy_settings,runinfo & paramotopy_info);
-	void make_master_from_recovered(std::map< int, point > successful_resolves);
-	void write_failed_paths(runinfo paramotopy_info, int iteration);
-	void find_successful_resolves(runinfo & paramotopy_info);
-	void write_successful_resolves(runinfo paramotopy_info);
-	void merge_successful_resolves(std::vector< point > current_successful_resolves, std::vector< int > relative_indices, std::vector< int > final_indices);
-	
-	void copy_step_one(std::string from_dir, std::string to_dir, int iteration, std::string startfilename);
-	
-	void report_failed_paths(runinfo paramotopy_info);
-	
-	
+  
+  /** Clear Data and rerun failed paths.
+      \param paramotopy_info - The current run information. 
+  */
+  void StartOver(runinfo & paramotopy_info);
+  /** Get the iteration number, recovers a list of points which have path failures, and reports the paths to the user. 
+      \param paramotopy_info The current run information. 
+  */
+  void RecoverProgress(runinfo & paramotopy_info);
+  /** Run an analysis on the failed paths.
+      \param paramotopy_settings - The paramotopy preference settings.
+      \param paramotopy_info - The current run info.
+  */
+  void PerformAnalysis(ProgSettings & paramotopy_settings, runinfo & paramotopy_info);
+  /** set the directory of the failed paths and the current pass. 
+   \param paramotopy_info - The current run information. */
+  void set_location_fail(runinfo & paramotopy_info);
+  /** 
+      Report to the user the initial fails and the terminal fails. 
+   */
+  void report_restored_data();
+  /** 
+      Parses a series of failed_paths files, searching for points which had such paths.
+      \param paramotopy_info - The current run info.
+      \param temporal_flag - 
+   */
+  int find_failed_paths(runinfo paramotopy_info, int temporal_flag, int iteration);
+  /** 
+      Set the vector of failed folders.
+      \param dir - The directory where the failed paths could exist in some subdirectory.
+   */
+  void get_folders_for_fail(std::string dir);
+
+  /** 
+      Write a new step1 file with different random points to run to the failed paths in the step 2 process.
+      \param paramotopy_settings - The paramotopy user defined settings.
+      \param paramotopy_info - The current run info.
+   */
+  std::string new_step_one(ProgSettings & paramotopy_settings,runinfo & paramotopy_info);
+  /** Not quite sure what this function does . . . 
+   Print the successful resolves and sort the fails and save them to the master_fails
+  \param successful_resolves - a map of the successful resolves. The int is the parameter number and the point is the parameter point.
+  */
+  void make_master_from_recovered(std::map< int, point > successful_resolves);
+
+  /**
+     Write the failed paths
+     \param paramotopy_info - The current run information. 
+     \param iteration - The current iteration for path failure resolution.
+  */
+  void write_failed_paths(runinfo paramotopy_info, int iteration);
+  void find_successful_resolves(runinfo & paramotopy_info);
+  void write_successful_resolves(runinfo paramotopy_info);
+  /** 
+      Merge the successful of resolved failed paths with the rest of the parameter points that have been successful.
+      \param current_successful_resolves - The current parameter points that had failed paths and are now resolved.
+      \param relative_indices - The relative indices (corresponding to the parameter point) of the points in current_successful_resolves. 
+      \param final_indices - The final indices of the points in current_successful_resolves.
+  */
+  void merge_successful_resolves(std::vector< point > current_successful_resolves, std::vector< int > relative_indices, std::vector< int > final_indices);
+  
+  /** 
+      Copy the step 1 file.
+      \param from_dir - The directory from which to copy the step1 file.
+      \param to_dir - The directory to copy the step1 file.
+      \param iteration - The current iteration number in the attempt to resolve path failures.
+      \param startfilename - The file to copy.
+   */
+  void copy_step_one(std::string from_dir, std::string to_dir, int iteration, std::string startfilename);
+  /** 
+      Report the failed paths to the user.
+      \param paramotopy_info - The current paramotopy run info.
+   */
+  void report_failed_paths(runinfo paramotopy_info);
+  
+  
 };
 
 
