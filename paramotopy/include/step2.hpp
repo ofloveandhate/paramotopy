76adfa380d6320716d2af511ab0199bdc80bac09
dani brake <ofloveandhate@users.noreply.github.com>
2013-05-09 23:46:11 +0000

    added step2 header file

---
+++ paramotopy/include/step2.hpp
@@ -0,0 +1,55 @@
+#include <mpi.h>
+
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+
+
+
+#define BOOST_FILESYSTEM_VERSION 3
+#define BOOST_FILESYSTEM_NO_DEPRECATED
+
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/progress.hpp>
+#include <boost/regex.hpp>
+
+
+#include "step1_funcs.hpp"
+#include "step2_funcs.hpp"
+#include "mtrand.hpp"
+#include "random.hpp"
+#include "step2readandwrite.hpp"
+#include "master.hpp"
+#include "slave.hpp"
+#include "para_aux_funcs.hpp"
+#include "timing.hpp"
+#include "paramotopy_enum.hpp"
+
+/*
+ the computeNumdenom function is in the bertini library.  it takes in a character array, and pointers which return the numerator
+ and denominator of the number.  I pass the input by casting via (char *)
+ */
+extern "C" {
+	void computeNumDenom(char **numer, char **denom, char *s);
+}
+
+
+extern "C" {
+	int bertini_main(int argC, char *args[]);
+}
+
+
+
+
